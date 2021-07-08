ff2e8db1ddf145ed84547348e56af245d2976235
Arianna <arianna.soriani@studio.unibo.it>
2021-07-08 09:56:58 +0200

    Home terminata

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import './App.css';
 import 'animate.css';
-import HeaderName from "./HeaderName";
-import './HeaderName.css';
-import Body from "./Body";
+
 //import { Helmet } from 'react-helmet';
 //const TITLE = 'Benvenuto'
 //<Helmet>
@@ -13,11 +11,31 @@ function App() {
     const name = 'Playa Majorca'
 
   return (
-      <html>
-        <HeaderName name={name}/>
-        <Body/>
+      <body>
+      <div class="row-container">
+          <div class={"menu"}>
+              <div className="menu-icon">
+                        <div class="bar1"></div>
+                        <div class="bar2"></div>
+                        <div class="bar3"></div>
+              </div>
+              <div class={"menu-M"}> Menu </div>
+          </div>
+          <div class={"title"}>
+              <h1 className="animate__animated animate__bounceInDown"> {name} </h1>
+          </div>
+          <div class={"right"}>
 
-      </html>
+          </div>
+      </div>
+      <div className="container">
+          <div className={"row"}>
+              PRENOTA SUBITO <br/>
+              IL TUO OMBRELLONE!
+          </div>
+      </div>
+      </body>
+
   );
 }
 
