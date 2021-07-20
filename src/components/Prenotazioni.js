bb7cfaa1e03bf53e4053eb88089feb3c95fdb0a4
Arianna <arianna.soriani@studio.unibo.it>
2021-07-20 11:32:22 +0200

    Tabella pg prenotazioni iniziata

--- src/components/Prenotazioni.js
+++ src/components/Prenotazioni.js
@@ -1,9 +1,43 @@
-function Prenotazioni() {
+import {FaUmbrellaBeach, BiBed} from "react-icons/all";
+import "./prenotazioni.css"
+import ScriptTag from 'react-script-tag'
+import React,{Component} from 'react'
+import {render} from "@testing-library/react";
+import Script from '@gumgum/react-script-tag'
+
+
+const Prenotazioni =() =>{
+
+    const _onMyScriptLoad=()=>{
+        const row = 8;
+        const column = 10;
+
+        for(let i=0; i<=row; i++){
+            document.write("<div className=\"row\" id=i>");
+            for(let j=0; j<=column; j++){
+                document.write("<div className=\"grid-item\" id=j>");
+                document.write("ciao");
+                document.write("</div>")
+            }
+            document.write("</div>")
+        }
+
+    };
+
+
     return (
         <body>
-            Prenotazioni
+        <div className="grid-container">
+            <Script
+                type="text/javascript"
+                onLoad={_onMyScriptLoad()}
+            />
+        </div>
         </body>
+
     )
+
+
 }
 
 export default Prenotazioni;
