5afc8760194c3427a5d6655a979ac2e3948bba01
Arianna <arianna.soriani@studio.unibo.it>
2023-01-11 18:02:57 +0100

    modifiche homepage

--- src/data/HomeData.js
+++ src/data/HomeData.js
@@ -1,5 +1,6 @@
 import ImageOne from '../STABILIMENTO-BALNEARE.jpg'
-import ImageTwo from '../Mare.jpg'
+import ImageTwo from '../ombrelloni.jpeg'
+import ImageThree from '../ombrelloni3.jpg'
 
 const name='Playa Majorca';
 const label='Prenota il tuo ombrellone';
@@ -15,6 +16,12 @@ export const HomeData = [
         title:name,
         path:'/booking',
         image: ImageTwo,
+        label:label
+    },
+    {
+        title:name,
+        path:'/booking',
+        image: ImageThree,
         label:label
     }
 ];
